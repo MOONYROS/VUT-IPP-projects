af27eaafeb256b71ba218fa58045d1509102d9ad
Ondrej Lukasek <ondrej@lukasek.cz>
2023-04-06 20:36:03 +0200

    Update main.py

--- proj2/main.py
+++ proj2/main.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
 import sys
+import re
 
 
 # definice konstant
@@ -9,7 +10,13 @@ errWrongXMLFormat = 31
 errUnexpectedStruct = 32
 
 errSemantic = 52
+errWrongOperandType = 53
+errGettingNotExistVar = 54
 errFrameNotExist = 55
+errMissingValue = 56
+errWrongOperandValue = 57
+errWrongStringOperation = 58
+
 class Frame:
     def __init__(self):
         self.dict = {}
@@ -19,14 +26,43 @@ class Frame:
         if not self.isInitialized:
             errorExit("Ramec neexistuje", errFrameNotExist)
 
+lineNr = 1
 
 class Runtime:
-    def __init__(self):
+    def __init__(self, srcName):
         self.GF = Frame()
         self.LF = Frame()
         self.TF = Frame()
         self.GF.isInitialized = True
         self.act_frame = self.GF
+        self.program = []
+        self.labels = {}
+        try:
+            self.parser = XMLParser(srcName) # spec_example.out
+        except ET.ParseError as e:
+            errorExit("Chyba pri parsovani XML", errWrongXMLFormat)
+
+    def getLinesLabels(self):
+        global lineNr
+        for instr in self.parser.root:
+            print(instr.attrib)
+            if int(instr.get("order", default=0)) != lineNr:
+                errorExit("Spatne cislo radku!", errUnexpectedStruct)
+            else:
+                lineNr += 1
+
+            opCode = instr.get("opcode", default="UNKNOWN")
+            arg1 = instr.find("arg1")
+            if arg1 == None:
+                errorExit("Nemame arg1!", errUnexpectedStruct)
+            arg1Text = arg1.text
+            order = instr.get("order", default=0)
+            self.program.append([order])
+            if opCode == "LABEL":
+                self.labels[arg1Text] = order
+
+    # def isVarName(self, varName):
+    #     return varName[:3] == "GF@" or varName[:3] == "LF@" or varName[:3] == "TF@"
 
     def checkFrameVar(self, varName):
         if varName[:3] == "GF@":
@@ -64,7 +100,113 @@ class Runtime:
         if not self.isVarDefined(varName):
             errorExit("Promenna neni definovana!", errSemantic)
         else:
+            if type == "string" and value == None:
+                value = ""
             self.act_frame.dict[self.actVarName] = value
+
+    def strHexToChar(self, inputString):
+        return re.sub(r'\\(\d{3})', lambda match: chr(int(match.group(1))), inputString)
+
+    def mustHaveArg2(self, arg2):
+        if arg2 == None:
+            errorExit("Nemame arg2!", errUnexpectedStruct)
+
+    def mustHaveArg23(self, arg2, arg3):
+        self.mustHaveArg2(arg2)
+        if arg3 == None:
+            errorExit("Nemame arg3!", errUnexpectedStruct)
+
+    def run(self):
+        i = 0
+        while i < len(RT.program):
+            line = RT.program[i][0]
+            # print("Processing line " + line)
+            instr = RT.parser.root.findall(".//*[@order='" + line + "']")[0]
+            opCode = instr.get("opcode", default="UNKNOWN")
+
+            arg1 = instr.find("arg1")
+            if arg1 == None:
+                errorExit("Nemame arg1!", errUnexpectedStruct)
+            arg1Text = arg1.text
+            arg1Type = arg1.get("type")
+            arg2 = instr.find("arg2")
+
+            if arg2 != None:
+                arg2Text = arg2.text
+                arg2Type = arg2.get("type")
+            else:
+                arg2Text = None
+                arg2Type = None
+            arg3 = instr.find("arg3")
+            if arg3 != None:
+                arg3Text = arg3.text
+                arg3Type = arg3.get("type")
+            else:
+                arg3Text = None
+                arg3Type = None
+
+            if opCode == "WRITE":
+                if arg1Type == "var":
+                    print(self.getVar(arg1Text), end="")
+                elif arg1Type == "string":
+                    print(self.strHexToChar(arg1Text), end="")
+                else:
+                    print( arg1Type + " " + arg1Text + " JESTE NEUMIM VYPSAT")
+            elif opCode == "DEFVAR":
+                RT.defineVar(arg1Text)
+            elif opCode == "MOVE":
+                self.mustHaveArg2(arg2)
+                RT.setVar(arg1Text, arg2.get("type"), arg2Text)
+            elif opCode == "LABEL":
+                i += 1  # TODO tady vyresit co s tim
+                continue
+            elif opCode == "JUMPIFEQ":
+                self.mustHaveArg23(arg2, arg3)
+                if arg2Type == "var":
+                    tmp1 = self.getVar(arg2Text)
+                elif arg2Type == "string":
+                    tmp1 = self.strHexToChar(arg2Text)
+                else:
+                    print(arg2Type + " " + arg2Text + " JESTE NEUMIM POROVNAVAT")
+                    tmp1 = ""
+                if arg3Type == "var":
+                    tmp2 = self.getVar(arg3Text)
+                elif arg3Type == "string":
+                    tmp2 = self.strHexToChar(arg3Text)
+                else:
+                    print(arg3Type + " " + arg3Text + " JESTE NEUMIM POROVNAVAT")
+                    tmp2 = ""
+
+                if tmp1 == tmp2:
+                    i = int(self.labels[arg1Text])
+                    continue
+            elif opCode == "CONCAT":
+                self.mustHaveArg23(arg2, arg3)
+                if arg1Type != "var":
+                    errorExit("Argument 1 neni VAR!", errWrongOperandType)
+                if arg2Type == "var":
+                    tmp1 = self.getVar(arg2Text)
+                elif arg2Type == "string":
+                    tmp1 = self.strHexToChar(arg2Text)
+                else:
+                    print(arg2Type + " " + arg2Text + " JESTE NEUMIM KONKATENOVAT")
+                    tmp1 = ""
+                if arg3Type == "var":
+                    tmp2 = self.getVar(arg3Text)
+                elif arg3Type == "string":
+                    tmp2 = self.strHexToChar(arg3Text)
+                else:
+                    print(arg3Type + " " + arg3Text + " JESTE NEUMIM KONKATENOVAT")
+                    tmp2 = ""
+
+                self.setVar(arg1Text, "string", tmp1 + tmp2)
+            elif opCode == "JUMP":
+                i = int(self.labels[arg1Text])
+                continue
+            else:
+                errorExit("Neznama intrukce!", errLexSynt)
+
+            i += 1
 
 
 class XMLParser:
@@ -81,67 +223,6 @@ def errorExit(message, errorNumber):
     sys.stderr.write(message + " On order number: " + str(lineNr))
     sys.exit(errorNumber)
 
-# Example usage
-try:
-    parser = XMLParser('spec_example.out')
-except ET.ParseError as e:
-    errorExit("Chyba pri parsovani XML", errWrongXMLFormat)
-    exit(0)
-
-lineNr = 1
-RT = Runtime()
-program = []
-
-for instr in parser.root:
-    print(instr.attrib)
-
-    if int(instr.get("order", default=0)) != lineNr:
-        errorExit("Spatne cislo radku!", errUnexpectedStruct)
-    else:
-        lineNr += 1
-
-    opCode = instr.get("opcode", default="UNKNOWN")
-    arg1 = instr.find("arg1")
-    if arg1 == None:
-        errorExit("Nemame arg1!", errUnexpectedStruct)
-    arg1Text = arg1.text
-    if opCode == "LABEL":
-        program.append([instr.get("order", default=0), arg1Text])
-    else:
-        program.append([instr.get("order", default=0), None])
-
-end = False
-i = 0
-
-while i < len(program):
-    line = program[i][0]
-    print("Processing line " + line)
-    instr = parser.root.findall(".//*[@order='" + line + "']")[0]
-    opCode = instr.get("opcode", default="UNKNOWN")
-
-    arg1 = instr.find("arg1")
-    if arg1 == None:
-        errorExit("Nemame arg1!", errUnexpectedStruct)
-    arg1Text = arg1.text
-
-    if opCode == "WRITE":
-        print(arg1Text)
-    elif opCode == "DEFVAR":
-        RT.defineVar(arg1Text)
-    elif opCode == "MOVE":
-        arg2 = instr.find("arg2")
-        if arg2 == None:
-            errorExit("Nemame arg2!", errUnexpectedStruct)
-        arg2Text = arg2.text
-        RT.setVar(arg1Text, arg2.get("type"), arg2Text)
-    # elif opCode == "LABEL": TODO Rozmyslet se co s tim
-    elif opCode == "JUMPIFEQ":
-        print("AHOJ")
-    elif opCode == "CONCAT":
-        print("AHOJ")
-    elif opCode == "JUMP":
-        print("AHOJ")
-    else:
-        errorExit("Neznama intrukce!", errLexSynt)
-
-    i += 1
+RT = Runtime("spec_example.out")
+RT.getLinesLabels()
+RT.run()
